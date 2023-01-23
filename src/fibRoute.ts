dc3976a58a374d93654bd5fc27002a3cf7e23691
ala <ala@aladembp.wifi.local.cmu.edu>
2023-01-23 10:58:40 -0500

    m

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,6 +1,8 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci from "./fib";
+
+import{Request,Response} from "express";
 
 export default (req, res) => {
   const { num } = req.params;
