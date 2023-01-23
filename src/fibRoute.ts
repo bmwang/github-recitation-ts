20d00402d6abb2807a4437377f7f367273bef994
bmwang <98133358+bmwang@users.noreply.github.com>
2023-01-23 12:10:33 -0500

    Revert "Fix fib route"

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,6 +1,6 @@
 // Endpoint for querying the fibonacci numbers
 
-import fibonacci from "./fib";
+import fibonacci = require("./fib");
 
 import express = require("express");
 import {Request, Response} from 'express';
@@ -8,7 +8,7 @@ import {Request, Response} from 'express';
 export default (req : Request, res : Response) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
+  const fibN:number = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
