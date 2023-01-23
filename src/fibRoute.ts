4dc57f71b9dff383b4fd5133c309625cf63ee186
bmwang <bw2@andrew.cmu.edu>
2023-01-23 11:23:04 -0500

    Fix fibRoute translation to ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,11 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci = require("./fib");
 
-export default (req, res) => {
+import express = require('express');
+import {Request, Response} from 'express';
+
+export default (req : Request, res : Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
